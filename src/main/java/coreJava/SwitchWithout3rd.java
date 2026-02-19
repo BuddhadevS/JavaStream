4925187e129bb07e497c66062d9f64a9af2a9ff3
buddhadevS <buddhadev0509@gmail.com>
2026-02-19 21:54:54 +0530

    buddha

---
+++ src/main/java/coreJava/SwitchWithout3rd.java
@@ -0,0 +1,9 @@
+package coreJava;
+
+public class SwitchWithout3rd {
+    public static void main(String[] args) {
+        String s = "Java";
+        s=s.toLowerCase();
+        System.out.println(s);
+    }
+}
