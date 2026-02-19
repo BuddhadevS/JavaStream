020c1a5ade452c53fa3069e9ab181ac7669d21eb
buddhadevS <buddhadev0509@gmail.com>
2026-02-19 22:18:34 +0530

    even number

---
+++ src/main/java/coreJava/Practice.java
@@ -0,0 +1,10 @@
+package coreJava;
+
+public class Practice {
+    public static void main(String[] args) {
+        int arr []={2,34,5,6,7,8};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
